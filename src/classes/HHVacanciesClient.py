17fe5992fb0490b2e4a1b21f0a9c9c38ff85f36a
RedKrane86 <redkrane86@gmail.com>
2024-12-27 17:49:10 +0500

    fix post_application, is worked, but has incorrect output

--- src/classes/HHVacanciesClient.py
+++ src/classes/HHVacanciesClient.py
@@ -1,5 +1,7 @@
 import aiohttp
 from typing import Any
+
+from async_property import async_property
 from fastapi import HTTPException
 
 ERRORS_LIST = [403, 404, 408, 410, 504, 500]
@@ -10,41 +12,39 @@ class HHVacanciesClient:
     def __init__(self, access_token: str) -> None:
         self.access_token = access_token
 
-    @property
-    def headers(self) -> dict[str, str]:
-        return {'Authorization': f'Bearer {self.access_token}",  "Content-Type": "application/json'}
+    @async_property
+    async def headers(self) -> dict[str, str]:
+        return {"Authorization": f"Bearer {self.access_token}"}
 
-    async def post_application(self, student_data) -> Any:
+    async def post_application(self, message, resume_id, vacancy_id) -> Any:
         url = 'https://api.hh.ru/negotiations'
-        headers = self.headers
+        headers = await self.headers
 
-        print(student_data)
-
-        post_body = {
-            'message': student_data['message'],
-            'resume_id': student_data['resume_id'],
-            'vacancy_id': student_data['vacancy_id']
-        }
+        form_data = aiohttp.FormData()
+        form_data.add_field(name='message', value=message)
+        form_data.add_field(name='resume_id', value=resume_id)
+        form_data.add_field(name='vacancy_id', value=vacancy_id)
 
         async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=headers, json=post_body, ssl=False) as response:
-                if response.status == 200:
-                    content = await response.json()
+            async with session.post(url, headers=headers, data=form_data, ssl=False) as response:
+                if response.status == 201:
+                    content = await response.json(content_type='text/html')
                     return content
                 elif response.status in ERRORS_LIST:
                     raise HTTPException(status_code=response.status)
 
+
     async def get_vacancy(self, hh_vacancy_id: str) -> str:
         url = f'https://api.hh.ru/vacancies/{hh_vacancy_id}'
+
+        # Хедеры вызывают ошибку 403 на api.hh.ru, временно отключено
         # headers = self.headers
 
         async with aiohttp.ClientSession() as session:
+            # Хедеры вызывают ошибку 403 на api.hh.ru, временно отключено
             async with session.get(url, ssl=False) as response:
                 if response.status == 200:
                     content = await response.json()
                     return content
                 elif response.status in ERRORS_LIST:
                     raise HTTPException(status_code=response.status)
-                # else:
-                #     content = await response.json()
-                #     return content
--- src/routers/vacancy.py
+++ src/routers/vacancy.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Form
 from starlette.requests import Request
 
 from src.classes.HHVacanciesClient import HHVacanciesClient
@@ -8,11 +10,10 @@ router = APIRouter()
 
 @router.post("/applications/")
 @check_access_token
-async def post_application(request: Request):
+async def post_application(request: Request, message: Annotated[str, Form()], resume_id: Annotated[str, Form()], vacancy_id: Annotated[str, Form()]):
     hh_access_token = request.headers['hh_access_token']
     hh_app_client = HHVacanciesClient(access_token=hh_access_token)
-    data = await request.json()
-    response = await hh_app_client.post_application(data)
+    response = await hh_app_client.post_application(message, resume_id, vacancy_id)
     return response
 
 @router.get("/vacancies/{hh_vacancy_id}/")
